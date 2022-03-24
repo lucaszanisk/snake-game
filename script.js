b476ded8c60580674f2c0b30b9314990bf2d016b
lucaszanisk <lucaszanisk@gmail.com>
2022-03-23 22:43:46 -0300

    game over, its done

--- script.js
+++ script.js
@@ -43,6 +43,13 @@ function initGame() {
   if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
   if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(game);
+      alert('game over ;(');
+    }
+  }
+
   createBG();
   createSnake();
   drawFood();
@@ -53,7 +60,12 @@ function initGame() {
   if (direction == 'left') snakeX -= box;
   if (direction == 'up') snakeY -= box;
   if (direction == 'down') snakeY += box;
-  snake.pop();
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
   let newHead = {
     x: snakeX,
     y: snakeY,
